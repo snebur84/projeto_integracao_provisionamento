865a73069b571a8c2bad7abf5d0aec8cdb043e52
Rubens Lemos <snebur84@gmail.com>
2025-10-18 23:32:30 +0000

    Atualizando deploy local

--- app/provision/core/urls.py
+++ app/provision/core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<int:pk>/edit/', views.DeviceUpdateView.as_view(), name='device_update'),
     path('<int:pk>/delete/', views.DeviceDeleteView.as_view(), name='device_delete'),
     path('export/csv/', views.export_devices_csv, name='device_export_csv'),
+    path('users/create/', views.UserCreateView.as_view(), name='user_create'),
 ]
