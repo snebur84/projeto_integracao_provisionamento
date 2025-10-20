32f1a4157121fb44bf1f4d7e3996ebda75e9cff6
Rubens Lemos <snebur84@gmail.com>
2025-10-20 14:01:21 +0000

    Simplificando o deploy em nuvem

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
