76e1d398a13da0cbb70ca9ec5f02c8c930f7f75e
Rubens Lemos <snebur84@gmail.com>
2025-10-20 12:09:47 +0000

    Correção para validação de token AWS

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = var.tfstate_bucket
-    key            = "${var.name_prefix}/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = var.tfstate_lock_table
-    encrypt        = true
-  }
+  backend "s3" {}
 }
