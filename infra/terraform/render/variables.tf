6573c5efaf789f1bdf58980a4463bb8309486bbe
Rubens Lemos <snebur84@gmail.com>
2025-10-26 12:45:52 +0000

    Mudando deploy em Render para uso do Postgres embarcado e mongoDB Atlas

--- infra/terraform/render/variables.tf
+++ infra/terraform/render/variables.tf
@@ -1,11 +1,3 @@
-variable "render_api_key" {
-  type = string
-}
-
-variable "render_owner_id" {
-  type = string
-}
-
 variable "service_name" {
   type    = string
   default = "provision" # ajuste para o nome real do serviço no Render
@@ -22,6 +14,18 @@ variable "create_postgres" {
   type    = bool
   default = true
 }
-variable "postgres_database" { type = string default = "provision_db" }
-variable "postgres_user" { type = string default = "provision_user" }
-variable "postgres_password" { type = string default = "" }
+
+variable "postgres_database" {
+  type    = string
+  default = "provision_db"
+}
+
+variable "postgres_user" {
+  type    = string
+  default = "provision_user"
+}
+
+variable "postgres_password" {
+  type    = string
+  default = ""
+}
